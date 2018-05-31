d0a3f2fd192b9f1879d07c0a3e05be210197b093
NinnYu <nychin92@gmail.com>
2018-05-31 12:50:34 -0400

    'commit

--- init.js
+++ init.js
@@ -1,8 +1,7 @@
 /*
 	Name: 			Dexter
 	Author: 		NinnYu Chin
-	Description: 	A game that gives random math questions and their answers. A new question
-	is made along with its answer with every click. AFter 100 clicks, the game is over.
+	Description: 	A game that gives random math qustions. The user can click a button to see the answer and click a different button to get the next question. It keeps count of how many questions has been given out.
 */
 
 var config = {
@@ -11,14 +10,13 @@ var config = {
     height: 400,
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create
     }
 };
 
 var game = new Phaser.Game(config);
-var clicked = 0;
-var clickedText;
+var counter = 1;
+var counterText;
 var deedeeText;
 var dexterText;
 var num1;
@@ -26,6 +24,11 @@ var num2;
 var question;
 var answer;
 var operator = ["+","-","*","/"];
+var answ_button;
+var next_button;
+var answ_text;
+var next_text;
+var gameover = false;
 
 function preload ()
 {
@@ -35,6 +38,7 @@ function preload ()
     this.load.image('deedee', 'assets/deedee.png');
     this.load.image('box1', 'assets/box1.png');
     this.load.image('box2', 'assets/box2.png');
+    this.load.spritesheet('button', 'assets/button_sprite.png', { frameWidth: 100, frameHeight: 50 });
 }
 
 function create ()
@@ -45,12 +49,18 @@ function create ()
     this.add.image(450, 240, 'deedee').setScale(0.5);
     this.add.image(300, 200, 'box1');
     this.add.image(300, 200, 'box2');
+    answ_button = this.add.sprite(200, 353, 'button').setInteractive();
+    next_button = this.add.sprite(320, 353, 'button').setInteractive();
 
     //Shows how many clicks.
-    clickedText = this.add.text(400, 10, 'Clicked: 0', {font: '28px Cooper Black', fill: '#fff'});
-    clickedText.stroke = "#de77ae";
-    clickedText.strokeThickness = 16;
-    clickedText.setShadow(2, 2, "#333333", 2, true, true);
+    counterText = this.add.text(400, 10, 'Question: ' + counter, {font: '28px Cooper Black', fill: '#fff'});
+    counterText.stroke = "#de77ae";
+    counterText.strokeThickness = 16;
+    counterText.setShadow(2, 2, "#333333", 2, true, true);
+
+    //Shows text on the two buttons.
+    answ_text = this.add.text(165, 333, "Click for\nanswer", {font: 'bold 16px Cooper', fill: '#000'});
+    next_text = this.add.text(285, 333, "Next\nQuestion", {font: 'bold 16px Cooper', fill: "#000"});
 
     //Randomly picks 2 numbers from 1-50 for the math question.
     num1 = Math.floor(Math.random() * 51)+1;
@@ -60,30 +70,42 @@ function create ()
     question = num1 + " " + operator[Math.floor(Math.random()*operator.length)] + " " + num2;
     deedeeText = this.add.text(70, 50, "What's " + question + " ?", {font: '32px Calibri', fill: '#000'});
 
-    //Shows the answer.
+    //Calculates answer.
     answer = eval(question).toFixed(1);
-    dexterText = this.add.text(150, 233, "Of course it's ... " + answer + " !", {font: '18px Calibri', fill: '#000'});
-	
-}
+    dexterText = this.add.text(160, 233, "You don't know?", {font: '20px Calibri', fill: '#000'});
 
-function update ()
-{
-	//Ends game when click number reaches 100.
-	if (clicked >= 100)
-	{
-		return;
-	}
+    //Shows the answer when the button is clicked.
+    answ_button.on('pointerdown', function (pointer) {
+        this.setFrame(1);
+        dexterText.setText("Of course it's... " + answer + " !");
+    });
+
+    answ_button.on('pointerout', function (pointer) {
+        this.setFrame(0);
+    });
+
+    answ_button.on('pointerup', function (pointer) {
+        this.setFrame(0);
+    });
 	
-	//Generates a new question and its answer when there's a click.
-	if (game.input.activePointer.isDown)
-	{
-		clicked += 1;
-		clickedText.setText('Clicked: ' + clicked);
-		num1 = Math.floor(Math.random() * 51)+1;
-   		num2 = Math.floor(Math.random() * 51)+1;
-		question = num1 + " " + operator[Math.floor(Math.random()*operator.length)] + " " + num2;
-		deedeeText.setText("What's " + question + " ?");
-		answer = eval(question).toFixed(1);
-    	dexterText.setText("Of course it's ... " + answer + " !");
-	}
+    //Shows the next question when the button is clicked.
+    next_button.on('pointerdown', function (pointer) {
+        counter++;
+        counterText.setText('Question: ' + counter);
+        num1 = Math.floor(Math.random() * 51)+1;
+        num2 = Math.floor(Math.random() * 51)+1;
+        question = num1 + " " + operator[Math.floor(Math.random()*operator.length)] + " " + num2;
+        deedeeText.setText("What's " + question + " ?");
+        answer = eval(question).toFixed(1);
+        dexterText.setText("You don't know?");
+        this.setFrame(1);
+    });
+
+    next_button.on('pointerout', function (pointer) {
+        this.setFrame(0);
+    });
+
+    next_button.on('pointerup', function (pointer) {
+        this.setFrame(0);
+    });
 }
